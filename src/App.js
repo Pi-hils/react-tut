55cdb7b5c4301e2d6fdf23042c346c79f2c10780
Hilda Amponsah <hilda.amp@hotmail.com>
2021-02-11 01:08:22 +0000

    Tut 2, creating a var, a function, adding buttons, words, and using js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 
 function App() {
+
+  var myName="Hilda Amponsah"
+
+  function alertingNames(){
+    alert(myName);
+  }
+
   return (
-    <div className="App">Hello World
+    <div className="App">
+    <button onClick={alertingNames}>Click Me</button>
+    <br></br>
+    <input type='text' />
+    <br></br>
+    <h1>Hello World, my name is {myName}</h1>
     </div>
   );
 }
